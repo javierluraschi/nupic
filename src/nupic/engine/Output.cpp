55570ccc43ca7f6a6a210c5c024c194978efe5f9
Javier Luraschi <javierluraschi@hotmail.com>
2019-10-14 01:27:33 -0700

    port remaining folders

---
+++ src/nupic/engine/Output.cpp
@@ -0,0 +1,121 @@
+/* ---------------------------------------------------------------------
+ * Numenta Platform for Intelligent Computing (NuPIC)
+ * Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+ * with Numenta, Inc., for a separate license for this software code, the
+ * following terms and conditions apply:
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *
+ * http://numenta.org/licenses/
+ * ---------------------------------------------------------------------
+ */
+
+/** @file
+ * Implementation of Output class
+ *
+ */
+
+#include <cstring>               // memset
+#include <nupic/engine/Link.hpp> // temporary
+#include <nupic/engine/Output.hpp>
+#include <nupic/engine/Region.hpp>
+#include <nupic/ntypes/Array.hpp>
+#include <nupic/types/BasicType.hpp>
+
+namespace nupic {
+
+Output::Output(Region &region, NTA_BasicType type, bool isRegionLevel,
+               bool isSparse)
+    : region_(region), isRegionLevel_(isRegionLevel), name_("Unnamed"),
+      nodeOutputElementCount_(0), isSparse_(isSparse) {
+  data_ = new Array(type);
+}
+
+Output::~Output() noexcept(false) {
+  // If we have any outgoing links, then there has been an
+  // error in the shutdown process. Not good to thow an exception
+  // from a destructor, but we need to catch this error, and it
+  // should never occur if nupic internal logic is correct.
+  NTA_CHECK(links_.size() == 0) << "Internal error in region deletion";
+  delete data_;
+}
+
+// allocate buffer
+void Output::initialize(size_t count) {
+  // reinitialization is ok
+  // might happen if initial initialization failed with an
+  // exception (elsewhere) and was retried.
+  if (data_->getBuffer() != nullptr)
+    return;
+
+  if (isSparse_) {
+    NTA_CHECK(isRegionLevel_) << "Sparse data must be region level";
+    NTA_CHECK(data_->getType() == NTA_BasicType_UInt32)
+        << "Sparse data must be uint32";
+  }
+
+  nodeOutputElementCount_ = count;
+  size_t dataCount;
+  if (isRegionLevel_)
+    dataCount = count;
+  else
+    dataCount = count * region_.getDimensions().getCount();
+  if (dataCount != 0) {
+    data_->allocateBuffer(dataCount);
+    // Zero the buffer because unitialized outputs can screw up inspectors,
+    // which look at the output before compute(). NPC-60
+    void *buffer = data_->getBuffer();
+    size_t byteCount = dataCount * BasicType::getSize(data_->getType());
+    memset(buffer, 0, byteCount);
+  }
+}
+
+void Output::addLink(Link *link) {
+  // Make sure we don't add the same link twice
+  // It is a logic error if we add the same link twice here, since
+  // this method should only be called from Input::addLink
+  auto linkIter = links_.find(link);
+  NTA_CHECK(linkIter == links_.end());
+
+  links_.insert(link);
+}
+
+void Output::removeLink(Link *link) {
+  auto linkIter = links_.find(link);
+  // Should only be called internally. Logic error if link not found
+  NTA_CHECK(linkIter != links_.end());
+  // Output::removeLink is only called from Input::removeLink so we don't
+  // have to worry about removing it on the Input side
+  links_.erase(linkIter);
+}
+
+const Array &Output::getData() const { return *data_; }
+
+bool Output::isRegionLevel() const { return isRegionLevel_; }
+
+Region &Output::getRegion() const { return region_; }
+bool Output::isSparse() const { return isSparse_; }
+
+void Output::setName(const std::string &name) { name_ = name; }
+
+const std::string &Output::getName() const { return name_; }
+
+size_t Output::getNodeOutputElementCount() const {
+  return nodeOutputElementCount_;
+}
+
+bool Output::hasOutgoingLinks() { return (!links_.empty()); }
+
+NTA_BasicType Output::getDataType() const { return data_->getType(); }
+
+} // namespace nupic
---
+++ src/nupic/engine/Spec.hpp
@@ -0,0 +1,155 @@
+/* ---------------------------------------------------------------------
+ * Numenta Platform for Intelligent Computing (NuPIC)
+ * Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+ * with Numenta, Inc., for a separate license for this software code, the
+ * following terms and conditions apply:
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ *
+ * http://numenta.org/licenses/
+ * ---------------------------------------------------------------------
+ */
+
+/** @file
+Definition of Spec data structures
+*/
+
+#ifndef NTA_SPEC_HPP
+#define NTA_SPEC_HPP
+
+#include <map>
+#include <nupic/ntypes/Collection.hpp>
+#include <nupic/types/Types.hpp>
+#include <string>
+
+namespace nupic {
+class InputSpec {
+public:
+  InputSpec() {}
+  InputSpec(std::string description, NTA_BasicType dataType, UInt32 count,
+            bool required, bool regionLevel, bool isDefaultInput,
+            bool requireSplitterMap = true, bool sparse = false);
+  bool operator==(const InputSpec &other) const;
+  inline bool operator!=(const InputSpec &other) const {
+    return !operator==(other);
+  }
+  std::string description;
+  NTA_BasicType dataType;
+  // TBD: Omit? isn't it always of unknown size?
+  // 1 = scalar; > 1 = array of fixed sized; 0 = array of unknown size
+  UInt32 count;
+  // TBD. Omit? what is "required"? Is it ok to be zero length?
+  bool required;
+  bool regionLevel;
+  bool isDefaultInput;
+  bool requireSplitterMap;
+  bool sparse;
+};
+
+class OutputSpec {
+public:
+  OutputSpec() {}
+  OutputSpec(std::string description, const NTA_BasicType dataType,
+             size_t count, bool regionLevel, bool isDefaultOutput,
+             bool sparse = false);
+  bool operator==(const OutputSpec &other) const;
+  inline bool operator!=(const OutputSpec &other) const {
+    return !operator==(other);
+  }
+  std::string description;
+  NTA_BasicType dataType;
+  // Size, in number of elements. If size is fixed, specify it here.
+  // Value of 0 means it is determined dynamically
+  size_t count;
+  bool regionLevel;
+  bool isDefaultOutput;
+  bool sparse;
+};
+
+class CommandSpec {
+public:
+  CommandSpec() {}
+  CommandSpec(std::string description);
+  bool operator==(const CommandSpec &other) const;
+  inline bool operator!=(const CommandSpec &other) const {
+    return !operator==(other);
+  }
+  std::string description;
+};
+
+class ParameterSpec {
+public:
+  typedef enum { CreateAccess, ReadOnlyAccess, ReadWriteAccess } AccessMode;
+
+  ParameterSpec() {}
+  /**
+   * @param defaultValue -- a JSON-encoded value
+   */
+  ParameterSpec(std::string description, NTA_BasicType dataType, size_t count,
+                std::string constraints, std::string defaultValue,
+                AccessMode accessMode);
+  bool operator==(const ParameterSpec &other) const;
+  inline bool operator!=(const ParameterSpec &other) const {
+    return !operator==(other);
+  }
+  std::string description;
+
+  // [open: current basic types are bytes/{u}int16/32/64, real32/64, BytePtr. Is
+  // this the right list? Should we have std::string, jsonstd::string?]
+  NTA_BasicType dataType;
+  // 1 = scalar; > 1 = array o fixed sized; 0 = array of unknown size
+  // TODO: should be size_t? Serialization issues?
+  size_t count;
+  std::string constraints;
+  std::string defaultValue; // JSON representation; empty std::string means
+                            // parameter is required
+  AccessMode accessMode;
+};
+
+struct Spec {
+  // Return a printable string with Spec information
+  // TODO: should this be in the base API or layered? In the API right
+  // now since we do not build layered libraries.
+  std::string toString() const;
+  bool operator==(const Spec &other) const;
+  inline bool operator!=(const Spec &other) const { return !operator==(other); }
+  // Some RegionImpls support only a single node in a region.
+  // Such regions always have dimension [1]
+  bool singleNodeOnly;
+
+  // Description of the node as a whole
+  std::string description;
+
+  Collection<InputSpec> inputs;
+  Collection<OutputSpec> outputs;
+  Collection<CommandSpec> commands;
+  Collection<ParameterSpec> parameters;
+
+#ifdef NTA_INTERNAL
+
+  Spec();
+
+  // TODO: decide whether/how to wrap these
+  std::string getDefaultOutputName() const;
+  std::string getDefaultInputName() const;
+
+  // TODO: need Spec validation, to make sure
+  // that default input/output are defined
+  // Currently this is checked in getDefault*, above
+
+#endif // NTA_INTERNAL
+};
+
+} // namespace nupic
+
+#endif // NTA_SPEC_HPP
