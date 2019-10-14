a65d54ea455b3302406a5b95c2befc333b16dabc
Javier Luraschi <javierluraschi@hotmail.com>
2019-10-14 03:06:35 -0700

    add integration test headers

--- src/rcpp_hello_world.cpp
+++ src/rcpp_hello_world.cpp
@@ -2,10 +2,28 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+#include <nupic/engine/Input.hpp>
+#include <nupic/engine/Link.hpp>
+#include <nupic/engine/Network.hpp>
+#include <nupic/engine/NuPIC.hpp>
+#include <nupic/engine/Output.hpp>
+#include <nupic/engine/Region.hpp>
+#include <nupic/engine/Spec.hpp>
+#include <nupic/ntypes/Array.hpp>
+#include <nupic/ntypes/ArrayRef.hpp>
+#include <nupic/ntypes/Dimensions.hpp>
+#include <nupic/os/Env.hpp>
+#include <nupic/os/OS.hpp> // memory leak detection
+#include <nupic/os/Path.hpp>
+#include <nupic/os/Timer.hpp>
 #include <nupic/types/Exception.hpp>
-#include <nupic/types/Fraction.hpp>
-#include <nupic/types/BasicType.hpp>
-#include <nupic/types/Types.hpp>
+
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 // [[Rcpp::export]]
 List rcpp_hello_world() {
