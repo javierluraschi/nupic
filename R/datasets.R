dc5fec254d3e22f69637748a336d176fd3a400d2
Javier Luraschi <javierluraschi@hotmail.com>
2019-10-17 23:04:26 +0200

    add support for nyc_taxi dataset

--- R/datasets.R
+++ R/datasets.R
@@ -6,7 +6,7 @@
 #'
 #' @format An object of class \code{"data.frame"}.
 #' @format A data frame with two variables: \code{timestamp} and
-#'   \code{kw_energy_consumption}
+#'   \code{consumption}
 #'
 #' @keywords datasets
 #'
@@ -16,3 +16,22 @@
 #' @examples
 #' data(gym_hourly)
 "gym_hourly"
+
+#' New York Taxis
+#' 
+#' @docType data
+#'
+#' @usage data(nyc_taxi)
+#'
+#' @format An object of class \code{"data.frame"}.
+#' @format A data frame with two variables: \code{timestamp} and
+#'   \code{value}
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://github.com/numenta/nupic}{Nupic}
+#'
+#' @keywords internal
+#' @examples
+#' data(nyc_taxi)
+"nyc_taxi"
--- tools/process-data.R
+++ tools/process-data.R
@@ -6,3 +6,11 @@ gym_hourly <- readr::read_csv(
   col_types = list(readr::col_datetime("%m/%d/%y %H:%M"), readr::col_double()))
 
 save(gym_hourly, file = "data/gym_hourly.rda")
+
+nyc_taxi <- readr::read_csv(
+  "../nupic/src/nupic/datafiles/extra/nycTaxi/nycTaxi.csv",
+  skip = 1,
+  col_names = c("timestamp", "value"),
+  col_types = list(readr::col_datetime("%Y-%m-%d %H:%M:%S"), readr::col_integer()))
+
+save(nyc_taxi, file = "data/nyc_taxi.rda")
