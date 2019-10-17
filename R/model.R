8d069a23482486743e293a81cff2c705408bf4cd
Javier Luraschi <javierluraschi@hotmail.com>
2019-10-17 14:17:00 +0200

    initialize metrics manager

--- R/model.R
+++ R/model.R
@@ -1,6 +1,32 @@
+metrics_manager <- function(model) {
+  metrics <- nupic$frameworks$opf$metrics
+
+  specs <- list(
+    metrics$MetricSpec(field = "consumption", metric = "multiStep",
+                       inferenceElement = "multiStepBestPredictions",
+                       params = list(errorMetric = "aae", window = 1000, steps = 1)),
+    metrics$MetricSpec(field = "consumption", metric = "trivial",
+                       inferenceElement ="prediction",
+                       params = list(errorMetric = "aae", window = 1000, steps = 1)),
+    metrics$MetricSpec(field = "consumption", metric = "multiStep",
+                       inferenceElement = "multiStepBestPredictions",
+                       params = list(errorMetric = "altMAPE", window = 1000, steps =  1)),
+    metrics$MetricSpec(field = "consumption", metric = "trivial",
+                       inferenceElement = "prediction",
+                       params = list(errorMetric = "altMAPE", window = 1000, steps = 1))
+  )
+  
+  nupic$frameworks$opf$prediction_metrics_manager$MetricsManager(
+    specs, model$getFieldInfo(), model$getInferenceType())
+}
+
 #' @export
 nupic <- function(params = nupic_example("hotgym")) {
   nupic <- import("nupic")
   
-  nupic$frameworks$opf$model_factory$ModelFactory$create(params)
+  model <- nupic$frameworks$opf$model_factory$ModelFactory$create(params)
+  
+  model$enableInference(list(predictedField = "consumption"))
+  
+  model
 }
